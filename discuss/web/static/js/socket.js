69b1a8651ec77ea3e8e6ca7e1b37324d9b073b60
CJFontaine <c.j.fontaine@gmail.com>
2019-05-08 21:23:52 -0400

    Finishing comments web socket

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -53,10 +53,56 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+  // Now that you are connected, you can join channels with a topic:
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { 
+      console.log(resp);
+      renderComments(resp.comments);
+    })
+    .receive("error", resp => { console.log("Unable to join", resp) })
 
-export default socket
+  channel.on(`comments:${topicId}:new`, renderComment);
+
+  document.querySelector('button').addEventListener('click', () => {
+    const content = document.querySelector('textarea').value;
+
+    channel.push('comment:add', { 
+      topicId: topicId,
+      content: content 
+    });
+  });
+}
+
+function commentTemplate(comment) {
+  let email = 'Anonymous';
+
+  if(comment.user) {
+    email = comment.user.email;
+  }
+
+  return `
+    <li class="collection-item">
+      ${comment.content}
+      <div class="secondary-content">
+        ${email}
+      </div>
+    </li>`;  
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+
+  document.querySelector('.collection').innerHTML += renderedComment;
+}
+
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return commentTemplate(comment);
+  });
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
+
+window.createSocket = createSocket;
